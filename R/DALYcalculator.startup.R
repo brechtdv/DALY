9b3d0b4517370c86779bb95bb086cbc10934e532
Brecht Devleesschauwer <Brecht.Devleesschauwer@UGent.be>
2013-07-28 17:51:38 +0200

    Update version to 1.2.1

--- R/DALYcalculator.startup.R
+++ R/DALYcalculator.startup.R
@@ -10,7 +10,7 @@ function(){
   setStdLE()
 
   ## Info message
-  info <- paste("DALY Calculator 1.2.0 (2013-07-06)",
+  info <- paste("DALY Calculator 1.2.1 (2013-07-28)",
                 "\nhttp://users.ugent.be/~bdvleess/DALYcalculator",
                 "\nDeveloped and maintained by:",
                 "  Brecht Devleesschauwer <Brecht.Devleesschauwer@UGent.be>",
